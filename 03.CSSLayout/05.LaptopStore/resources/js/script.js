2ff52cd90f7497df34dbf7268d3e1003971d0208
vassildinev <vassildinev@gmail.com>
2015-05-14 07:59:03 +0300

    Added CSS Repo.

---
+++ 03.CSSLayout/05.LaptopStore/resources/js/script.js
@@ -0,0 +1,16 @@
+function min_price_slider_showValue(newValue)
+{
+	document.getElementById("min_price_slider_value").innerHTML=newValue;
+}
+function max_price_slider_showValue(newValue)
+{
+	document.getElementById("max_price_slider_value").innerHTML=newValue;
+}
+function min_ram_slider_showValue(newValue)
+{
+	document.getElementById("min_ram_slider_value").innerHTML=newValue;
+}
+function max_ram_slider_showValue(newValue)
+{
+	document.getElementById("max_ram_slider_value").innerHTML=newValue;
+}
